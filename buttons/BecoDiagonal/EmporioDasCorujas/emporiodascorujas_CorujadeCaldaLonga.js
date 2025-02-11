eda9fb0bf8bc6c06c5ef557001d6a35f63896a38
Pedro Cardoso <pmcardoso.bcl@gmail.com>
2025-02-11 16:14:10 +0000

    .

--- buttons/BecoDiagonal/EmporioDasCorujas/emporiodascorujas_CorujadeCaldaLonga.js
+++ buttons/BecoDiagonal/EmporioDasCorujas/emporiodascorujas_CorujadeCaldaLonga.js
@@ -19,16 +19,16 @@ module.exports = {
             return interaction.reply({ content: `Você não tem Galeões suficientes`, ephemeral: true });
         }
 
-        if(userInv.inventario.animal) {
-            return interaction.reply({ content: `Você já comprou 1 animal de companhia`, ephemeral: true });
-        }else{
-            userInv.inventario.galeoes.amount -= price;
-            userInv.inventario.animal= {name: name, type: 'owl', img: img, amount: 1}
-            
-            fs.writeFileSync(`./RPGData/players/inv_${RemoveSpecialCharacters(member.user.username)}_${member.user.id}.json`, JSON.stringify(userInv));
-            const embed = new EmbedBuilder().setColor('#ffad00').setImage(img).setDescription(`Você comprou 1 ${name}`);
-            
-            return interaction.reply({  embeds: [embed], ephemeral: true });
+        if (userInv.inventario.animal) {
+            userInv.inventario.animal = null
+            userInv.inventario.galeoes.amount += 10
         }
+        userInv.inventario.galeoes.amount -= price;
+        userInv.inventario.animal = { name: name, type: 'owl', img: img, amount: 1 }
+
+        fs.writeFileSync(`./RPGData/players/inv_${RemoveSpecialCharacters(member.user.username)}_${member.user.id}.json`, JSON.stringify(userInv));
+        const embed = new EmbedBuilder().setColor('#ffad00').setImage(img).setDescription(`Você comprou 1 ${name}`);
+
+        return interaction.reply({ embeds: [embed], ephemeral: true });
     }
 }
