a5b67a3555238666cd006f4a2d21fa617e5bdb0c
Pedro Cardoso <pmcardoso.bcl@gmail.com>
2025-02-23 22:12:55 +0000

    .

--- commands/ficha.js
+++ commands/ficha.js
@@ -7,6 +7,7 @@ const { RemoveSpecialCharacters, FichaToPDF, createFichaEmbed } = require('../ut
 const spell_list = JSON.parse(fs.readFileSync(`./RPGData/spell_list.json`, 'utf8'))
 const desvantagem_list = JSON.parse(fs.readFileSync(`./RPGData/desvantagem_list.json`, 'utf8'))
 const vantagem_list = JSON.parse(fs.readFileSync(`./RPGData/vantagem_list.json`, 'utf8'))
+const vantagem_desvantagem_by_year = JSON.parse(fs.readFileSync(`./RPGData/vantagem_desvantagem_by_year.json`, 'utf8'))
 
 const fichaCampos = {
     name: 'Nome',
@@ -69,9 +70,19 @@ module.exports = {
             .setDescription('[DM] Adicionar/Remover PE ao player')
             .addStringOption(option => option.setName('player').setDescription('Marque o player. EX: @fulano').setRequired(true))
             .addStringOption(option => option.setName('pontos').setDescription('Pontos').setRequired(true))
+        )
+        .addSubcommand(command => command.setName('vantagem')
+            .setDescription('[DM] Alterar lista de Vantagens de player')
+            .addStringOption(option => option.setName('player').setDescription('Marque o player. EX: @fulano').setRequired(true))
+            .addStringOption(option => option.setName('vantagens').setDescription('Lista de vantagens (separado por virgula)').setRequired(true))
+        )
+        .addSubcommand(command => command.setName('desvantagem')
+            .setDescription('[DM] Alterar lista de Desvantagens de player')
+            .addStringOption(option => option.setName('player').setDescription('Marque o player. EX: @fulano').setRequired(true))
+            .addStringOption(option => option.setName('desvantagens').setDescription('Lista de desvantagens (separado por virgula)').setRequired(true))
         ),
     async execute(interaction, client) {
-        await interaction.deferReply({ ephemeral: true }).catch( () => {} );
+        await interaction.deferReply({ ephemeral: true }).catch(() => { });
 
         const guild = client.guilds.cache.get(interaction.guildId);
         const member = guild.members.cache.get(interaction.user.id);
@@ -88,12 +99,12 @@ module.exports = {
                 player_file = fs.readFileSync(`./RPGData/players/ficha_personagem/ficha_personagem_${RemoveSpecialCharacters(user.username)}_${user.id}.json`, 'utf8');
                 ficha_player = JSON.parse(player_file)
 
-                await interaction.editReply({ embeds: createFichaEmbed(user.username, user.id) , ephemeral: true });
+                await interaction.editReply({ embeds: createFichaEmbed(user.username, user.id), ephemeral: true });
 
                 interaction.followUp({ content: 'Gerando ficha em PDF. Poderá demorar alguns segundos', ephemeral: true });
 
 
-                await FichaToPDF(member.user.username,member.user.id)
+                await FichaToPDF(member.user.username, member.user.id)
                 const filePathDoc = path.join('./tempdata/', `ficha_${RemoveSpecialCharacters(member.user.username)}_${member.user.id}.docx`);
                 const filePathPdf = path.join('./tempdata/', `ficha_${RemoveSpecialCharacters(member.user.username)}_${member.user.id}.pdf`);
 
@@ -123,15 +134,15 @@ module.exports = {
 
                     interaction.followUp({ content: 'Gerando ficha em PDF. Poderá demorar alguns segundos', ephemeral: true });
 
-                    await FichaToPDF(player_user.user.username,player_user.user.id)
+                    await FichaToPDF(player_user.user.username, player_user.user.id)
                     const filePathDoc = path.join('./tempdata/', `ficha_${RemoveSpecialCharacters(player_user.user.username)}_${player_user.user.id}.docx`);
                     const filePathPdf = path.join('./tempdata/', `ficha_${RemoveSpecialCharacters(player_user.user.username)}_${player_user.user.id}.pdf`);
-    
+
                     attachment = new AttachmentBuilder(filePathPdf);
                     interaction.followUp({ files: [attachment], ephemeral: true });
-    
+
                     await new Promise(resolve => setTimeout(resolve, 5000));
-    
+
                     fs.unlinkSync(filePathDoc);
                     fs.unlinkSync(filePathPdf);
                 }
@@ -158,7 +169,7 @@ module.exports = {
                     ficha_player.PV += pv
                     if (ficha_player.PV <= 0) {
                         ficha_player.PV = 0
-                    }else if(ficha_player.PV > ficha_player.PVMax){
+                    } else if (ficha_player.PV > ficha_player.PVMax) {
                         ficha_player.PV = ficha_player.PVMax
                     }
 
@@ -220,7 +231,7 @@ module.exports = {
                     ficha_player.PM += pm
                     if (ficha_player.PM <= 0) {
                         ficha_player.PM = 0
-                    }else if(ficha_player.PM > ficha_player.PMMax){
+                    } else if (ficha_player.PM > ficha_player.PMMax) {
                         ficha_player.PM = ficha_player.PMMax
                     }
 
@@ -285,7 +296,79 @@ module.exports = {
 
                     fs.writeFileSync(`./RPGData/players/ficha_personagem/ficha_personagem_${RemoveSpecialCharacters(player_user.user.username)}_${player_user.user.id}.json`, JSON.stringify(ficha_player));
 
-                    interaction.editReply({ content: `Player **${player_user.nickname || player_user.user.globalName || player_user.user.username}**\n${ficha_player[pe]} atual é ${ficha_player[pe]} `, ephemeral: true });
+                    interaction.editReply({ content: `Player **${player_user.nickname || player_user.user.globalName || player_user.user.username}**\nPontos de Experiência atual é ${ficha_player[pe]} `, ephemeral: true });
+                }
+
+                break
+            case 'vantagem':
+                if (isDM) {
+                    player = interaction.options.getString('player');
+                    playerID = player.match(/\d+/)[0]; // Get the user ID from the mention
+                    player_user = guild.members.cache.get(playerID); // Get the member object from the ID
+                    const vantagens = Number(interaction.options.getString('vantagens'));
+
+                    if (!player_user) {
+                        return interaction.editReply({ content: `Player **${player}** não existe.`, ephemeral: true });
+                    }
+
+                    const vantagenslist = vantagens.split(',').map(v => v.trim());
+                    if (vantagenslist.length > 3) {
+                        return interaction.editReply({ content: `Escolha no máximo até 3 vantagens.`, ephemeral: true });
+                    }
+                    for (let i = 0; i < vantagenslist.length; i++) {
+                        const vantagem = vantagenslist[i]
+                        if (!Object.values(vantagem_list).map(v => v.label.toUpperCase()).includes(vantagem.toUpperCase())) {
+                            return interaction.editReply({ content: `${vantagem} não existe na lista`, ephemeral: true });
+                        }
+                    }
+
+                    player_file = fs.readFileSync(`./RPGData/players/ficha_personagem/ficha_personagem_${RemoveSpecialCharacters(player_user.user.username)}_${player_user.user.id}.json`, 'utf8');
+                    ficha_player = JSON.parse(player_file)
+
+                    ficha_personagem.vantagens = []
+                    vantagenslist.forEach(vantagem => {
+                        const id = Object.keys(vantagem_list).find(key => vantagem_list[key].label.toUpperCase() === vantagem.toUpperCase());
+                        ficha_personagem.vantagens.push(id)
+                    })
+
+                    fs.writeFileSync(`./RPGData/players/ficha_personagem/ficha_personagem_${RemoveSpecialCharacters(player_user.user.username)}_${player_user.user.id}.json`, JSON.stringify(ficha_player));
+                    interaction.editReply({ content: `Player **${player_user.nickname || player_user.user.globalName || player_user.user.username}**\nLista de Vantagens atual é ${vantagens} `, ephemeral: true });
+                }
+
+                break
+            case 'devantagem':
+                if (isDM) {
+                    player = interaction.options.getString('player');
+                    playerID = player.match(/\d+/)[0]; // Get the user ID from the mention
+                    player_user = guild.members.cache.get(playerID); // Get the member object from the ID
+                    const devantagens = Number(interaction.options.getString('devantagens'));
+
+                    if (!player_user) {
+                        return interaction.editReply({ content: `Player **${player}** não existe.`, ephemeral: true });
+                    }
+
+                    const devantagenslist = devantagens.split(',').map(v => v.trim());
+                    if (devantagenslist.length > 3) {
+                        return interaction.editReply({ content: `Escolha no máximo até 3 desvantagens.`, ephemeral: true });
+                    }
+                    for (let i = 0; i < devantagenslist.length; i++) {
+                        const desvantagem = devantagenslist[i]
+                        if (!Object.values(desvantagem_list).map(v => v.label.toUpperCase()).includes(desvantagem.toUpperCase())) {
+                            return interaction.editReply({ content: `${desvantagem} não existe na lista`, ephemeral: true });
+                        }
+                    }
+
+                    player_file = fs.readFileSync(`./RPGData/players/ficha_personagem/ficha_personagem_${RemoveSpecialCharacters(player_user.user.username)}_${player_user.user.id}.json`, 'utf8');
+                    ficha_player = JSON.parse(player_file)
+
+                    ficha_personagem.desvantagens = []
+                    devantagenslist.forEach(desvantagem => {
+                        const id = Object.keys(vantagem_list).find(key => desvantagem_list[key].label.toUpperCase() === desvantagem.toUpperCase());
+                        ficha_personagem.desvantagens.push(id)
+                    })
+
+                    fs.writeFileSync(`./RPGData/players/ficha_personagem/ficha_personagem_${RemoveSpecialCharacters(player_user.user.username)}_${player_user.user.id}.json`, JSON.stringify(ficha_player));
+                    interaction.editReply({ content: `Player **${player_user.nickname || player_user.user.globalName || player_user.user.username}**\nLista de Desvantagens atual é ${devantagens} `, ephemeral: true });
                 }
 
                 break
