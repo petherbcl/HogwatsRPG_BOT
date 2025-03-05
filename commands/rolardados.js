d128436fa53e847d83186768a4a2b11d82e65b2b
Pedro Cardoso <pmcardoso.bcl@gmail.com>
2025-03-05 23:23:14 +0000

    .

--- commands/rolardados.js
+++ commands/rolardados.js
@@ -31,7 +31,7 @@ module.exports = {
         .setName('rolardados')
         .setDescription('Simula a rolagem de dados.')
         .addStringOption(option => option.setName('dados').setDescription('Notação dos dados para rolar (ex: 3D20 2D6)').setRequired(true)),
-    async execute(interaction) {
+    async execute(interaction, client) {
 
         const guild = client.guilds.cache.get(interaction.guildId);
         const member = guild.members.cache.get(interaction.user.id);
